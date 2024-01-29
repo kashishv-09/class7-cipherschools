dc3d53bf5fa408ae72554fb02e5a7ce30cc0465d
Kashish Verma <kashish.v1209@gmail.com>
2024-01-29 19:48:01 +0530

    first commit

---
+++ js1.js
@@ -0,0 +1,91 @@
+// let var const
+//funstion declaration
+// function expression
+// hoisting
+//data types(imp)
+   //primitives
+      //1. string
+      //2. number
+      //3.boolean
+      //4.null
+      //5.bigint
+      //6.undefined
+      //7.symbol
+      // type of to check datatype
+
+      // its a bug in js
+       // console.log(typeof null );
+
+
+       // backward compatible language
+       // old features are still availabe and js is not 
+       // not a forward compatible language because after2004 if the chrome is not updated its new features of 2023 will not be accessible to you
+
+   //references
+       //ARRAY
+       //object
+       //function
+         
+
+    //    function hello(){
+    //     console.log("hello world");
+    //    }
+
+    //    let myArray=["1",2,hello.[1,2,3]];
+
+
+       //create using[]
+       //you can pass anything in array
+       //how to access value
+
+
+    //    console.log(myArray[2][1]);
+    //    console.log(myArray[5]);//undefined
+    //    const lastindex=myArray.length-1;  
+    
+    
+    // let fruits=["apple","mango","grapes"];
+    // const poopeditem=fruits.pop(); // remove from end
+    // const poopeditem=fruits.shift(); // remove from beginning
+    // fruits.push("blueberry");//add to beginning
+    // fruits.unshift("blueberry");// add to beginning
+
+    // push and pop= O(1)
+    //shift and unshift=O(n)
+
+    // // difference between primitive and reference datatype
+    // primitive datatypes are stored in stacks and refernce type are stored in heap
+    
+
+    //operators
+    //output based questions
+    // objects
+      //c++,c,java,python
+       //object class instance
+      // javascript- same as pyhton dict, java/c++ unordered map
+        
+
+      //array- ordered collection
+      // object-key:value pairs/unordered
+
+    //   const person={
+    //     "firstname":"John",
+    //     "Lastname":"Doe",
+    //     "age":25,
+    //     "new property":"unique val"
+    //   }
+    //   console.log(person);
+
+    //   //access properties
+    //   //1.dot notation
+    //   //2. bracket notation
+
+    //   console.log(person.age)
+    //   console(person["firstname"])
+
+
+    //loop
+   
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+
+    
